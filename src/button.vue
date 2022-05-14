e84aac02813387816c6b8b043c9c285578e68d8b
hezhen <hezhen20@foxmail.com>
2022-05-14 17:31:01 +0800

    添加 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,30 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <!-- slot 无法加类名，用 div 包起来 -->
+    <div class="content">
+      <slot>按钮</slot>
+    </div>
+  </button>
 </template>
 <script>
 export default {
-  
+  props: {
+    icon: {
+      type: String,
+      default: function() {
+        return ''
+      }
+    },
+    iconPosition: {
+      type: String,
+      default: function() {
+        return 'left'
+      }
+    }
+  }
 }
 </script>
 <style lang="scss">
@@ -22,6 +43,17 @@ export default {
   }
   &:focus {
     outline: none;
+  }
+  // 通过控制类名 以及flex 布局来调整 icon 和 文字的左右关系
+  display: inline-flex; justify-content: center; align-items: center;
+  vertical-align: middle;   // 解决内联元素垂直不对齐的问题
+  &.icon-left {
+    > .icon { order: 1; margin-right: .2em; }
+    > .content { order: 2; }
+  }
+  &.icon-right {
+    > .content { order: 1; }
+    > .icon { order: 2; margin-left: .2em; }
   }
 }
 </style>
