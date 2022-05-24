165cd8b8270a4555737e9132a5874c796ffa6ff9
hezhen <hezhen20@foxmail.com>
2022-05-24 17:33:06 +0800

    layout组件

---
+++ src/content.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="content">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+  .content {
+    flex-grow: 1;   // 就可以让 content 撑大
+  }
+</style>
