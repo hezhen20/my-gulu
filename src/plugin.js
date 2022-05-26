cb2996b64af73d2df5ffb4a88872697ac208bf30
hezhen <hezhen20@foxmail.com>
2022-05-26 20:22:27 +0800

    只允许有一个toast（避免重叠）

--- src/plugin.js
+++ src/plugin.js
@@ -1,14 +1,26 @@
 import Toast from './toast.vue'
+
+let currentToast
+
 export default {
   install(Vue, option) {
     Vue.prototype.$toast = function(message, toastOptions) {
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData: toastOptions,
-      })
-      toast.$slots.default = [message]
-      toast.$mount()  // 加载，但是未显示
-      document.body.appendChild(toast.$el)
+      if (currentToast) {
+        console.log('旧的toast已关闭');
+        currentToast.close()
+      }
+      currentToast = creatToast({Vue, message, propsData: toastOptions})
     }
   }
+}
+
+function creatToast({Vue, message, propsData}) {
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({
+    propsData
+  })
+  toast.$slots.default = [message]
+  toast.$mount()  // 加载，但是未显示
+  document.body.appendChild(toast.$el)
+  return toast
 }
